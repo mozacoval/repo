ab48727717751eb4bff9456f775a1d6703a38bdd
MozacOval <mozacoval@gmail.com>
2012-12-01 20:32:40 +0800

    some mods

---
+++ ClassAOSA.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ClassAOSA.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/01/2012 08:18:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "ClassAOSA.h"
+
+
+
+
+
---
+++ ClassAOSA.h
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ClassAOSA.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/01/2012 08:18:48 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#ifndef CLASSAOSA
+#define CLASSAOSA
+
+class ClassAOSA {
+    ClassAOSA();
+    virtual ~ClassAOSA();
+    
+    public:
+
+    private:
+
+
+    private:
+};
+
+
+#endif
+
